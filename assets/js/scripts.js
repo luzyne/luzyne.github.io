e74b9c2e36641d19838936830718eb582ca56237
brianlsf <brian.lsf@gmail.com>
2021-12-21 00:34:22 -0500

    post gallery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -191,3 +191,49 @@ if (st > 300){
 } else {
     document.getElementById("site-header").classList.remove("scrolled");
 }
+
+
+
+/*
+const ele = document.getElementById('post-gallery');
+
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+const mouseDownHandler = function (e) {
+    ele.style.cursor = 'grabbing';
+    ele.style.userSelect = 'none';
+    ele.classList.add('dragging');
+    pos = {
+        // The current scroll
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        // Get the current mouse position
+        x: e.clientX,
+        y: e.clientY,
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+};
+
+const mouseMoveHandler = function (e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+
+    // Scroll the element
+    ele.scrollTop = pos.top - dy;
+    ele.scrollLeft = pos.left - dx;
+};
+
+const mouseUpHandler = function () {
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+
+    ele.style.cursor = 'grab';
+    ele.style.removeProperty('user-select');
+    ele.classList.remove('dragging');
+};
+
+ele.addEventListener('mousedown', mouseDownHandler);
+*/
