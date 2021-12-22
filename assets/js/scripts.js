af15c204b9400f620e1e1cb8e533abb9f5eb46cb
brianlsf <brian.lsf@gmail.com>
2021-12-22 10:49:40 -0500

    gallery drag fix for barba js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -144,23 +144,96 @@ function init(){
 
     function initAfterBarba() {
 
+        if (document.getElementById('post-gallery')) {
+            // Slider dragging
+            const slider = document.getElementById('post-gallery');
+            let isDown = false;
+            let startX;
+            let scrollLeft;
+
+            slider.addEventListener('mousedown', (e) => {
+                isDown = true;
+                slider.classList.add('active');
+                startX = e.pageX - slider.offsetLeft;
+                scrollLeft = slider.scrollLeft;
+                cancelMomentumTracking();
+            });
+
+            slider.addEventListener('mouseleave', () => {
+                isDown = false;
+                slider.classList.remove('active');
+            });
+
+            slider.addEventListener('mouseup', () => {
+                isDown = false;
+                slider.classList.remove('active');
+                beginMomentumTracking();
+            });
+
+            slider.addEventListener('mousemove', (e) => {
+                if (!isDown) return;
+                e.preventDefault();
+                const x = e.pageX - slider.offsetLeft;
+                const walk = (x - startX); //scroll-fast
+                var prevScrollLeft = slider.scrollLeft;
+                slider.scrollLeft = scrollLeft - walk;
+                velX = slider.scrollLeft - prevScrollLeft;
+            });
+
+            // Momentum 
+            var velX = 0;
+            var momentumID;
+
+            slider.addEventListener('wheel', (e) => {
+                cancelMomentumTracking();
+            });
+
+            function beginMomentumTracking() {
+                cancelMomentumTracking();
+                momentumID = requestAnimationFrame(momentumLoop);
+            }
+
+            function cancelMomentumTracking() {
+                cancelAnimationFrame(momentumID);
+            }
+
+            function momentumLoop() {
+                slider.scrollLeft += velX * 2;
+                velX *= 0.95;
+                if (Math.abs(velX) > 0.5) {
+                    momentumID = requestAnimationFrame(momentumLoop);
+                }
+            }
+
+            document.getElementById("post-gallery").addEventListener("scroll", function(){
+               var st = this.scrollLeft;
+               if (st > 100){
+                    document.getElementById("gallery-container").classList.add("scrolled");
+               } else {
+                    document.getElementById("gallery-container").classList.remove("scrolled");
+               }
+            }, false);
+        }
+
         const iframe = document.querySelector('iframe');
         const player = new Vimeo.Player(iframe);
 
-        document.getElementById("play-video").onclick = function() {
-            player.play()
-            document.querySelector("meta[name='theme-color']").setAttribute("content", "#0000fa")
-        };
+        if (document.getElementById('play-video')) {
+            document.getElementById("play-video").onclick = function() {
+                player.play()
+                document.querySelector("meta[name='theme-color']").setAttribute("content", "#0000fa")
+            };
 
-        document.getElementById("stop-video").onclick = function() {
-            player.pause()
-            document.querySelector("meta[name='theme-color']").setAttribute("content", "#000")
-        };
+            document.getElementById("stop-video").onclick = function() {
+                player.pause()
+                document.querySelector("meta[name='theme-color']").setAttribute("content", "#000")
+            };
 
-        document.getElementById("close-video").onclick = function() {
-            player.pause()
-            document.querySelector("meta[name='theme-color']").setAttribute("content", "#000")
-        };
+            document.getElementById("close-video").onclick = function() {
+                player.pause()
+                document.querySelector("meta[name='theme-color']").setAttribute("content", "#000")
+            };
+        }
 
     };
     initAfterBarba();
